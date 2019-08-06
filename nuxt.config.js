d0b72c714d0c28af4c5f1a63c4850842ad3224e4
Luis <lalrg@outlook.com>
2019-08-05 21:18:58 -0600

    moved css to assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: ['element-ui/lib/theme-chalk/index.css', '~/css/main.css'],
+  css: ['element-ui/lib/theme-chalk/index.css', '~/assets/css/main.css'],
   /*
    ** Plugins to load before mounting the App
    */
